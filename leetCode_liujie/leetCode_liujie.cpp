3f2ad36589dd91282a9083eed0885d9b7c793c63
奥利奥 <369121232@qq.com>
2022-03-07 20:41:03 +0800

    添加项目文件。

---
+++ leetCode_liujie/leetCode_liujie.cpp
@@ -0,0 +1,67 @@
+﻿// leetCode_liujie.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	string simplifyPath(string path) {
+		auto split = [](const string& s, char delim) -> vector<string> {
+			vector<string> ans;
+			string cur;
+			for (char ch : s) {
+				if (ch == delim) {
+					ans.push_back(move(cur));
+					cur.clear();
+				}
+				else {
+					cur += ch;
+				}
+			}
+			ans.push_back(move(cur));
+			return ans;
+		};
+
+		vector<string> names = split(path, '/');
+		vector<string> stack;
+		for (string& name : names) {
+			if (name == "..") {
+				if (!stack.empty()) {
+					stack.pop_back();
+				}
+			}
+			else if (!name.empty() && name != ".") {
+				stack.push_back(move(name));
+			}
+		}
+		string ans;
+		if (stack.empty()) {
+			ans = "/";
+		}
+		else {
+			for (string& name : stack) {
+				ans += "/" + move(name);
+			}
+		}
+		return ans;
+	}
+};
+int main()
+{
+	Solution s;
+	string str = s.simplifyPath("/home/bin/../");
+
+	std::cout << str << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
